7884976c1c9830150c7b71a68a29e454c336b9e4
Kenneth Ruan <kennethjruan@gmail.com>
2022-09-17 18:50:37 -0400

    API Linking

--- frontend/src/Components/ScheduleContainer.js
+++ frontend/src/Components/ScheduleContainer.js
@@ -1,61 +1,74 @@
+import { useEffect } from 'react';
 import PatientProfile from './PatientProfile';
 import './ScheduleContainer.css';
 
 export default function ScheduleContainer(props){
     let {isDisplaying} = props
 
-    //dummy JSON
-    const dummyJSON = [
-        {
-            timeSlot:"10:00 am-11:00 am",
-            patientName:"Aava",
-            travelTime:"9min",
-            description:"kahsgohaf;oaushfiuah sfkhsakdfhkjashdf lkhaskdfhkalshklfhs",
-            extraInfo:"oasdh fluahs;ofgh;las dfhg kljahsdklhlksdjf"
-        },
-        {
-            timeSlot:"10:00 am-11:00 am",
-            patientName:"Aava",
-            travelTime:"9min",
-            description:"kahsgohaf;oaushfiuah sfkhsakdfhkjashdf lkhaskdfhkalshklfhs",
-            extraInfo:"oasdh fluahs;ofgh;las dfhg kljahsdklhlksdjf"
-        },
-        {
-            timeSlot:"10:00 am-11:00 am",
-            patientName:"Aava",
-            travelTime:"9min",
-            description:"kahsgohaf;oaushfiuah sfkhsakdfhkjashdf lkhaskdfhkalshklfhs",
-            extraInfo:"oasdh fluahs;ofgh;las dfhg kljahsdklhlksdjf"
-        },
-        {
-            timeSlot:"10:00 am-11:00 am",
-            patientName:"Aava",
-            travelTime:"9min",
-            description:"kahsgohaf;oaushfiuah sfkhsakdfhkjashdf lkhaskdfhkalshklfhs",
-            extraInfo:"oasdh fluahs;ofgh;las dfhg kljahsdklhlksdjf"
-        },
-        {
-            timeSlot:"10:00 am-11:00 am",
-            patientName:"Aava",
-            travelTime:"9min",
-            description:"kahsgohaf;oaushfiuah sfkhsakdfhkjashdf lkhaskdfhkalshklfhs",
-            extraInfo:"oasdh fluahs;ofgh;las dfhg kljahsdklhlksdjf"
-        },
-        {
-            timeSlot:"10:00 am-11:00 am",
-            patientName:"Aava",
-            travelTime:"9min",
-            description:"kahsgohaf;oaushfiuah sfkhsakdfhkjashdf lkhaskdfhkalshklfhs",
-            extraInfo:"oasdh fluahs;ofgh;las dfhg kljahsdklhlksdjf"
-        },
-        {
-            timeSlot:"10:00 am-11:00 am",
-            patientName:"Aava",
-            travelTime:"9min",
-            description:"kahsgohaf;oaushfiuah sfkhsakdfhkjashdf lkhaskdfhkalshklfhs",
-            extraInfo:"oasdh fluahs;ofgh;las dfhg kljahsdklhlksdjf"
-        }
-    ]
+    const PatientData = props => {
+        const [patientData, setPatientData] = useState([])
+
+        useEffect(() => {
+            fetch("/api/appointments",
+                { params: {
+                    user: props.user
+                }})
+                .then(res => res.json())
+                .then(data => setPatientData(data))
+        })
+    }
+    
+    // const dummyJSON = [
+    //     {
+    //         timeSlot:"10:00 am-11:00 am",
+    //         patientName:"Aava",
+    //         travelTime:"9min",
+    //         description:"kahsgohaf;oaushfiuah sfkhsakdfhkjashdf lkhaskdfhkalshklfhs",
+    //         extraInfo:"oasdh fluahs;ofgh;las dfhg kljahsdklhlksdjf"
+    //     },
+    //     {
+    //         timeSlot:"10:00 am-11:00 am",
+    //         patientName:"Aava",
+    //         travelTime:"9min",
+    //         description:"kahsgohaf;oaushfiuah sfkhsakdfhkjashdf lkhaskdfhkalshklfhs",
+    //         extraInfo:"oasdh fluahs;ofgh;las dfhg kljahsdklhlksdjf"
+    //     },
+    //     {
+    //         timeSlot:"10:00 am-11:00 am",
+    //         patientName:"Aava",
+    //         travelTime:"9min",
+    //         description:"kahsgohaf;oaushfiuah sfkhsakdfhkjashdf lkhaskdfhkalshklfhs",
+    //         extraInfo:"oasdh fluahs;ofgh;las dfhg kljahsdklhlksdjf"
+    //     },
+    //     {
+    //         timeSlot:"10:00 am-11:00 am",
+    //         patientName:"Aava",
+    //         travelTime:"9min",
+    //         description:"kahsgohaf;oaushfiuah sfkhsakdfhkjashdf lkhaskdfhkalshklfhs",
+    //         extraInfo:"oasdh fluahs;ofgh;las dfhg kljahsdklhlksdjf"
+    //     },
+    //     {
+    //         timeSlot:"10:00 am-11:00 am",
+    //         patientName:"Aava",
+    //         travelTime:"9min",
+    //         description:"kahsgohaf;oaushfiuah sfkhsakdfhkjashdf lkhaskdfhkalshklfhs",
+    //         extraInfo:"oasdh fluahs;ofgh;las dfhg kljahsdklhlksdjf"
+    //     },
+    //     {
+    //         timeSlot:"10:00 am-11:00 am",
+    //         patientName:"Aava",
+    //         travelTime:"9min",
+    //         description:"kahsgohaf;oaushfiuah sfkhsakdfhkjashdf lkhaskdfhkalshklfhs",
+    //         extraInfo:"oasdh fluahs;ofgh;las dfhg kljahsdklhlksdjf"
+    //     },
+    //     {
+    //         timeSlot:"10:00 am-11:00 am",
+    //         patientName:"Aava",
+    //         travelTime:"9min",
+    //         description:"kahsgohaf;oaushfiuah sfkhsakdfhkjashdf lkhaskdfhkalshklfhs",
+    //         extraInfo:"oasdh fluahs;ofgh;las dfhg kljahsdklhlksdjf"
+    //     }
+    // ]
     return(
         <div className="schedule-container" style={{display:(isDisplaying?"flex":"none")}}>
             {
