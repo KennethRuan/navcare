0a2224fa1fad79aa1e05efc583f4cad0f2b23242
Kenneth Ruan <kennethjruan@gmail.com>
2022-09-17 17:59:57 -0400

    Created database models for PSWs and clients

--- backend/psw/models.py
+++ backend/psw/models.py
@@ -1,4 +1,6 @@
+from tkinter import CASCADE
 from django.db import models
+from django.core.validators import MaxValueValidator
 
 # Create your models here.
 
@@ -6,7 +8,32 @@ class Client(models.Model):
     name = models.CharField(max_length=120)
     address = models.TextField()
     description = models.TextField()
-    completed = models.BooleanField(default=False)
 
-    def _str_(self):
+    def __str__(self):
         return self.name
+
+# Right now it only stores one longitude and latitude, but ideally it should have diff values depending on the time
+class PSW(models.Model):
+    name = models.CharField(max_length=120, unique=True)
+    latitude = models.DecimalField(max_digits=9, decimal_places=6)
+    longitude = models.DecimalField(max_digits=9, decimal_places=6)
+
+    def __str__(self):
+        return self.name
+
+class ScheduleItem(models.Model):
+    psw = models.ForeignKey(PSW, db_index=True, on_delete=models.CASCADE)
+    date = models.CharField(max_length = 8)
+    client = models.ForeignKey('Client', on_delete=models.CASCADE)
+    latitude = models.DecimalField(max_digits=9, decimal_places=6)
+    longitude = models.DecimalField(max_digits=9, decimal_places=6)
+
+    # Date is subdivided into 96, 15 minute intervals
+    start_time = models.PositiveSmallIntegerField(validators=[MaxValueValidator(96)])
+    end_time = models.PositiveSmallIntegerField(validators=[MaxValueValidator(96)])
+
+    def __str__(self):
+        return str(self.client) + "_" + str(self.date)
+
+
+
--- backend/psw/views.py
+++ backend/psw/views.py
@@ -2,9 +2,18 @@ from django.shortcuts import render
 from rest_framework import viewsets
 from .serializers import ClientSerializer
 from .models import Client
+from .helper import schedule
+from django.http import HttpResponse
+
 
 # Create your views here.
 
 class ClientView(viewsets.ModelViewSet):
     serializer_class = ClientSerializer
     queryset = Client.objects.all()
+
+def main(request):
+
+    schedule()
+    
+    return HttpResponse("Processed the closest PSWs")
