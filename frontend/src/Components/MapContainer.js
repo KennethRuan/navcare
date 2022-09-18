65fbcb921dc9e234d40c45d265d3c9e3ab42c812
aavaSapkota <sapkota.aava04@gmail.com>
2022-09-18 00:15:44 -0400

    google bug fix

--- frontend/src/Components/MapContainer.js
+++ frontend/src/Components/MapContainer.js
@@ -7,8 +7,11 @@ import {
   useLoadScript,
   Marker,
   InfoWindow,
-  DirectionsRenderer
+  DirectionsRenderer,
+  withGoogleMap
 } from '@react-google-maps/api'
+
+let google = window.google;
 
 // define literals to avoid lots of rerendering
 const libraries = ["places"]; 
@@ -20,10 +23,10 @@ const center = {
 
 export default function MapContainer (props) {
   let {isDisplaying} = props
-  const { isLoaded, loadError } = useLoadScript({
-    googleMapsApiKey: 'AIzaSyAeufE-n5QFRUQU3TlBoKXxqNHmmCl-oEw',
-    libraries
-  })
+  // const { isLoaded, loadError } = useLoadScript({
+  //   googleMapsApiKey: 'AIzaSyAeufE-n5QFRUQU3TlBoKXxqNHmmCl-oEw',
+  //   libraries
+  // })
 
   const [map, setMap] = useState(/** @type google.maps.Map */ null)
   const [directionsResponse, setDirectionsResponse] = useState(null)
@@ -41,16 +44,23 @@ export default function MapContainer (props) {
       stopover: true
     };
   });
-
-  console.log(routesCopy);
-
+  const [origin, setOrigin] = useState(null);
+  const [destination, setDestination] = useState(null);
+  const [waypoints, setwaypoints] = useState(null);
+  
+  // console.log(routesCopy);
+  
+  // useEffect(() => {
+  //   // console.log(loadError);
+  //   console.log("isloaded", isLoaded);
+  //   // console.log(loadError);
+  //   if(isLoaded){
+  //   }
+  // }, [isLoaded]);
+  
   useEffect(() => {
-    // console.log(loadError);
-    console.log("isloaded", isLoaded);
-    if(isLoaded){
-      calculateRoute();
-    }
-  }, [isLoaded, loadError]);
+    calculateRoute();
+  }, []);
 
   useEffect(() => {
     console.log(map);
@@ -60,25 +70,35 @@ export default function MapContainer (props) {
   // if(!isLoaded) return "Loading Maps";
 
   async function calculateRoute () {
-    const directionsService = new google.maps.DirectionsService()
-    const results = await directionsService.route({
-      origin: {
-        lat: 43.6532,
-        lng: -79.3832
-      },
-      destination: {
-        lat: 43.6532,
-        lng: -79.3838
-      },
-      // eslint-disable-next-line no-undef
-      travelMode: google.maps.TravelMode.DRIVING, 
-      waypoints: routesCopy,
-    })
-    console.log(results);
-    setDirectionsResponse(results)
+
+    if(routesCopy.length==1){
+      setOrigin(routesCopy[0]); //change to home address of psw
+      setDestination(routesCopy[0]);   
+    }else{
+      setOrigin(routesCopy[0]); 
+      setDestination(routesCopy[routesCopy.length-1]); 
+      setwaypoints(routesCopy.slice(0, -1));
+    }
+  
     // setDistance(results.routes[0].legs[0].distance.text)
     // setDuration(results.routes[0].legs[0].duration.text)
   }
+
+  useEffect(() => {
+    async function setup() {
+      const directionsService = new google.maps.DirectionsService() 
+      const results = await directionsService.route({
+        origin: routesCopy[0],
+        destination: routesCopy[routesCopy.length-1],
+        // eslint-disable-next-line no-undef
+        travelMode: google.maps.TravelMode.DRIVING, 
+        waypoints: routesCopy,
+      })
+      console.log(results);
+      setDirectionsResponse(results);
+    }
+    setup();
+  }, [origin, destination, waypoints])
 
   function displayRoutes(){
 
@@ -95,7 +115,7 @@ export default function MapContainer (props) {
   return (
     <div className="map-container" style={{display:(isDisplaying?"flex":"none")}}>
         <span>Next Appointment </span>
-    {isLoaded ? (
+    
       <div style={{ height: "300px", width: "100%" }}>
         <GoogleMap
           center={center}
@@ -110,16 +130,12 @@ export default function MapContainer (props) {
           }}
           onLoad={map => setMap(map)}
         >
-          <Marker position={center} />
-          {directionsResponse && (
-            <DirectionsRenderer directions={directionsResponse} />
-          )}
+          {/* <Marker position={center} /> */}
           {directionsResponse && (
             <DirectionsRenderer directions={directionsResponse} />
           )}
         </GoogleMap>
       </div>
-    ) : (<div><h1>test</h1></div>)}
     </div>
   )
 }
